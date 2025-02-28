7d9b557f8cb5e6925f284936b17155be60cfddad
hjcedeno <93888943+IsmaelYumi@users.noreply.github.com>
2025-02-28 15:38:32 -0500

    Cmabios en el .env

--- mi-app-courier/backend/routes/conecction.js
+++ mi-app-courier/backend/routes/conecction.js
@@ -5,7 +5,12 @@ dotenv.config();
 // Carga la configuración desde el archivo JSON
 const serviceAccount = require("../bd_firestore/clave.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
 
 // Inicializa Firestore
